d6c340a654fedb7f677e2d41283380e4e2199892
Nicolas <nicolassegond0@gmail.com>
2025-11-21 16:08:32 +0100

    fix: change test order

--- tests/article.test.js
+++ tests/article.test.js
@@ -1,36 +1,9 @@
-process.env.NODE_ENV = 'main';
-
 let articleId;
 
 // tests/article.test.js
 const request = require('supertest');
 const app = require('../app');
 const sqlite3 = require('sqlite3').verbose();
-
-
-    // Tests pour l'endpoint GET /articles
-    describe("API Articles - GET /articles", () => {
-        // Test pour vérifier que l'endpoint retourne la liste des articles avec les propriétés attendues
-        test('devrait retourner la liste des articles & tester si nom, prix et stock sont présents', async () => {
-            const response = await request(app).get('/articles');
-
-            // Vérifier le statut de la réponse & que le corps de la réponse est un tableau
-            expect(response.status).toBe(200);
-            expect(Array.isArray(response.body)).toBe(true);
-
-            // Vérifier que chaque article a les propriétés 'id', 'nom', 'prix' et 'stock'
-            const article = response.body[0];
-            expect(article).toHaveProperty('id');
-            expect(article).toHaveProperty('nom');
-            expect(article).toHaveProperty('prix');
-            expect(article).toHaveProperty('stock');
-
-            // Vérifier le type de chaques propriétés
-            expect(typeof article.nom).toBe('string');
-            expect(typeof article.prix).toBe('number');
-            expect(typeof article.stock).toBe('number');
-        });
-    });
 
     process.env.NODE_ENV = 'test';
 
@@ -66,6 +39,30 @@ const sqlite3 = require('sqlite3').verbose();
                 done();
             }
         );
+    });
+
+    // Tests pour l'endpoint GET /articles
+    describe("API Articles - GET /articles", () => {
+        // Test pour vérifier que l'endpoint retourne la liste des articles avec les propriétés attendues
+        test('devrait retourner la liste des articles & tester si nom, prix et stock sont présents', async () => {
+            const response = await request(app).get('/articles');
+
+            // Vérifier le statut de la réponse & que le corps de la réponse est un tableau
+            expect(response.status).toBe(200);
+            expect(Array.isArray(response.body)).toBe(true);
+
+            // Vérifier que chaque article a les propriétés 'id', 'nom', 'prix' et 'stock'
+            const article = response.body[0];
+            expect(article).toHaveProperty('id');
+            expect(article).toHaveProperty('nom');
+            expect(article).toHaveProperty('prix');
+            expect(article).toHaveProperty('stock');
+
+            // Vérifier le type de chaques propriétés
+            expect(typeof article.nom).toBe('string');
+            expect(typeof article.prix).toBe('number');
+            expect(typeof article.stock).toBe('number');
+        });
     });
 
     // ==================== TEST UPDATE ====================
